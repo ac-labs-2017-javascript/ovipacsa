2493da8029af10bef127a90c45b7c1acd15ed4dd
ovipacsa <vfsdvdsfsdbvf>
2017-03-28 18:04:57 +0300

    cewfweq

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello world");
+var express=require("express");
+var app = express();
+var fetch=require("node-fetch");
+var cheerio=require("cheerio");
+
+function protext(text) {
+  var $ =cheerio.load(text);
+  var listitems= Array.from($("#tiles").children("li"));
+  return listitems.map(function(li){
+    return {
+      title : $(li).find(".title").text()
+    };
+  });
+
+}
+
+app.get("/hello", function(req, res){
+  var dopopoco=fetch("http://www.dopopoco.ro/meniu-individual-timisoara");
+  var textResponse = dopopoco.then(function(response){
+    return response.text()
+  });
+  textResponse.then(function(text){
+    res.send(protext(text));
+});
+});
+
+app.listen(3000, function(){
+  console.log("mesasage");
+});
